63f29cae7fe408f7b44d2d8771f5aaf3380d4e31
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-13 17:17:56 -0700

    Simple interface

--- common/interface.ts
+++ common/interface.ts
@@ -1,3 +1,16 @@
-export interface Server {
-    hello(name: string): string
+import { Server, Client } from '../lib/rpc'
+
+export interface RPC {
+  hello(name: string): string
 }
+
+class RPCImpl implements RPC {
+  hello(name: string): string {
+    return `Hello World, ${name}`
+  }
+}
+
+const implRPC = new RPCImpl()
+const server = new Server<RPC>(implRPC)
+const client = Client<RPC>(server)
+console.log(client.hello("hi"))
