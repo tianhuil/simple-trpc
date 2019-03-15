1ed95a32b5719b7c211c8a99f9414dbb10b997c3
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-15 00:24:51 -0700

    simplify server

--- lib/server.ts
+++ lib/server.ts
@@ -1,23 +1,12 @@
 import { deserializeFunc, serializeResult, FuncCall } from "./utils"
 
-function parseInput(arg: string | object): FuncCall {
-  switch(typeof arg) {
-    case "string": {
-      return deserializeFunc(arg)
-    }
-    case "object": {
-      return arg as FuncCall
-    }
-  }
-}
-
 export class Server<T extends object> {
   implementation: T;
   constructor(implementation: T) {
     this.implementation = implementation;
   }
-  async handle(arg: string | object) {
-    const { name, args } = parseInput(arg)
+  async handle(arg: string) {
+    const { name, args } = deserializeFunc(arg)
     const result = await (this.implementation as any)[name](...args);
     return serializeResult<T>({ name, result });
   }
