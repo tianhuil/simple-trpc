fd56d30c1465b3275b8c9c881df73eca90b0aa87
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-16 14:17:12 -0700

    Adding in handler imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { directConnector, httpConnector, makeClient } from './client'
+import { registerExpressHandler } from './handler/express'
+import { registerKoaHandler } from './handler/koa'
+import { Handler } from './handler/handler'
 
-export { directConnector, httpConnector, makeClient }
+export { directConnector, httpConnector, makeClient, registerExpressHandler, registerKoaHandler, Handler }
