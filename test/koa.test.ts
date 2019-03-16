ddd50469e122ce047b90596bef07c923f3389621
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-16 00:01:39 -0700

    Revert "remove koa test"
    This reverts commit dee9f3849758db67609e07933bb8391bd135ab70.

---
+++ test/koa.test.ts
@@ -0,0 +1,22 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+import { RPCImpl } from '../example/implementation'
+import { IRPC } from '../example/interface'
+import { Client, httpConnector } from '../src/client'
+import { registerHandler } from '../src/handler/koa'
+import { testClient } from './utils'
+
+const PORT = 4850
+
+beforeAll(() => {
+  const implementation = new RPCImpl()
+  const app = new Koa()
+  const router = new Router()
+  registerHandler(app, router, implementation)
+  app.use(router.routes())
+  app.listen(PORT)
+})
+
+const client = Client<IRPC>(httpConnector(`http://localhost:${PORT}/`))
+
+testClient('Koa tests', client)
