d1f1d7c406dce8adfc5b92d757069b68e86b31db
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-15 22:38:22 -0700

    koa handler

--- example/koa.ts
+++ example/koa.ts
@@ -4,7 +4,7 @@ import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import Router from 'koa-router'
 
-import { Handler } from '../src/handler'
+import { registerHandler } from '../src/handler/koa'
 import { RPCImpl } from './implementation'
 import { IRPC } from './interface'
 
@@ -13,20 +13,14 @@ const port = 4001
 const implementation = new RPCImpl()
 
 const app = new Koa()
-app.use(bodyParser({
-  enableTypes: ['text'],
-}))
 const router = new Router()
-const handler = new Handler<IRPC>(implementation)
 
 router
   .get('/', async ({response}: Koa.Context) => {
     response.body = 'Hello World!'
   })
-  .post('/rpc', async ({request, response}: Koa.Context) => {
-    const result = await handler.handle(request.body as string)
-    response.body = result
-  })
+
+registerHandler<IRPC>(app, router, implementation)
 
 app.use(router.routes())
 app.listen(port, () => console.log(`Koa app listening on port ${port}!`))
---
+++ src/handler/koa.ts
@@ -0,0 +1,27 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import Router from 'koa-router'
+
+import { Handler } from '.'
+
+export function registerHandler<T extends object>(
+  app: Koa,
+  router: Router,
+  implementation: T,
+  textBodyParser = true,
+): Koa {
+  if (textBodyParser) {
+    app.use(bodyParser({
+      enableTypes: ['text'],
+    }))
+  }
+
+  const handler = new Handler<T>(implementation)
+
+  router.post('/rpc', async ({request, response}: Koa.Context) => {
+    const result = await handler.handle(request.body as string)
+    response.body = result
+  })
+
+  return app
+}
