7bf816a4c4f9c622ad63ed36732d59b0416cf19e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-15 23:58:48 -0700

    fix express test

--- test/express.test.ts
+++ test/express.test.ts
@@ -6,14 +6,15 @@ import { registerHandler } from '../src/handler/express'
 import { testClient } from './utils'
 
 const PORT = 9482
-
-beforeAll(() => {
-  const implementation = new RPCImpl()
-  const app: express.Application = express()
-  registerHandler(app, implementation)
-  app.listen(PORT)
-})
+const implementation = new RPCImpl()
+const app: express.Application = express()
+registerHandler(app, implementation)
+const server = app.listen(PORT)
 
 const client = Client<IRPC>(httpConnector(`http://localhost:${PORT}/`))
 
 testClient('Express tests', client)
+
+afterAll(() => {
+  server.close()
+})
