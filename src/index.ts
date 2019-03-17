af08ac39f84282ebbf331a0833ccf0971e98b156
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-16 17:32:55 -0700

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { directConnector, httpConnector, makeClient } from './client'
 import { registerExpressHandler } from './handler/express'
-import { registerKoaHandler } from './handler/koa'
 import { Handler } from './handler/handler'
+import { registerKoaHandler } from './handler/koa'
 
 export { directConnector, httpConnector, makeClient, registerExpressHandler, registerKoaHandler, Handler }
