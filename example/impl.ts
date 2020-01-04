d2ef1473fe8c81819468e09cd5b099f03ffb4775
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-01-03 19:51:24 -0800

    A few tweaks

--- example/impl.ts
+++ example/impl.ts
@@ -2,8 +2,8 @@ import { data, error } from '../src/util'
 import { IExampleRPC } from './interface'
 
 export class ExampleRPCImpl implements IExampleRPC {
-  public arithmetic {
-    public add = async (x: number, y: number) => data(x + y)
+  public arithmetic = {
+    add: async (x: number, y: number) => data(x + y)
   }
   public hello = async (name: string) => data(`Hello World, ${name}`)
   public user = async (id: number) => data({id, name: 'Bob ' + id})
